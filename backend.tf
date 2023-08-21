e39d6f153bb1b90050e9a3b92e98a9e06d128167
reshmadharmik <134468361+reshmadharmik@users.noreply.github.com>
2023-08-21 13:13:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terra-first-project"
+    bucket = "terraform-automation-bucket-project1"
     key = "terra"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
